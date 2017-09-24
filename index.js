34c5b07189b447a7f2f0a8e6efdea446f42b8f7d
robs898 <rmackenzie8@gmail.com>
2017-09-24 10:48:12 +0100

    changable values

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function getLocalCoins(selector) {
   for (var i = 0, row; row = coinTable.rows[i]; i++) {
     var d = {};
     d.symbol = row.cells[0].innerText;
-    d.amount = parseFloat(row.cells[1].innerText);
+    d.amount = parseFloat(row.cells[1].getElementsByTagName("input")[0].value);
     a.push(d);
   };
   return a;
